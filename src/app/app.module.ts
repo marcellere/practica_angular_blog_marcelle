117ca5710e407423406d7a237f01db407aa881fa
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-11-09 13:13:25 +0100

    all necessary components and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { PostFormComponent } from './pages/post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
