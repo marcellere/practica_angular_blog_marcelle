be9fbe15fa64cceb751777b51e50b55b2fff338d
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-11-09 13:24:42 +0100

    created methods create, getAll and getByCategory in posts service

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { IPost } from '../interfaces/post.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  arrPosts: IPost[] = []
+
+  create(post: IPost) {
+    this.arrPosts.push(post)
+  }
+
+  getAll(): IPost[] {
+    return this.arrPosts;
+  }
+
+  getByCategory(cat: string): IPost[] {
+    return this.arrPosts.filter(post => post.category === cat)
+  }
 }
