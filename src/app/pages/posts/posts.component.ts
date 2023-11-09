cbdb003a969b6101e19069cdaba670e9cbb47644
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-11-09 13:30:26 +0100

    created methods inside posts component

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IPost } from 'src/app/interfaces/post.interface';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class PostsComponent {
 
+  postsService = inject(PostsService)
+  arrPosts: IPost[] = []
+
+  ngOnInit() {
+    this.arrPosts = this.postsService.getAll()
+  }
+
+  onClickFilter(cat: string) {
+    if (cat !== '') {
+      this.arrPosts = this.postsService.getByCategory(cat)
+    } else {
+      this.arrPosts = this.postsService.getAll()
+    }
+  }
 }
