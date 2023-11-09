3f695e0908ef9746b1b66a6f59a8993d96899fc6
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-11-09 18:44:00 +0100

    images added to post array

--- src/app/data/posts.db.ts
+++ src/app/data/posts.db.ts
@@ -6,7 +6,7 @@ export const catBlogPosts: IPost[] = [
         title: "Adorable Kittens: A Purr-fect Delight",
         content: "Dive into the enchanting world of adorable kittens! These little furballs are not just cute; they're a purr-fect delight that can brighten even the gloomiest of days. From their playful antics to the heart-melting purrs that resonate through your soul, kittens are undeniably a source of joy and happiness.\n\nIn this blog post, we'll explore the irresistible charm of kittens, featuring snapshots of their cutest moments. Get ready to embark on a journey filled with fluffy cuddles, tiny paws, and endless smiles!",
         author: "CatLover123",
-        image: "https://example.com/kitten1.jpg",
+        image: "https://wgme.com/resources/media/1b738326-8a60-4c9c-8a1b-0cd6eb873833-medium16x9_kitten.jpg?1493772853243",
         date: new Date("2023-11-01"),
         category: "Kitten Cuteness"
     },
@@ -14,7 +14,7 @@ export const catBlogPosts: IPost[] = [
         title: "The Charm of Cat Naps",
         content: "Have you ever wondered about the secret world of cat naps? Cats seem to have mastered the art of relaxation, turning even the simplest snooze into a captivating event. In this blog post, we'll delve into the charm of cat naps and explore the undeniable allure these feline creatures bring to their sleepy adventures.\n\nJoin us as we unravel the mystery behind a cat's ability to find the coziest spots for a nap, the adorable positions they twist themselves into, and the pure joy that radiates from their sleepy, content faces. Get ready for a delightful dose of relaxation and cuteness!",
         author: "SnugglePaws",
-        image: "https://example.com/catnap2.jpg",
+        image: "https://bloximages.newyork1.vip.townnews.com/themountaineer.com/content/tncms/assets/v3/editorial/2/b9/2b9a8d86-7d2f-11eb-8cab-d32cf0dcca38/60414ea758731.image.jpg?resize=1396%2C962",
         date: new Date("2023-10-25"),
         category: "Cat Lifestyle"
     },
@@ -22,7 +22,7 @@ export const catBlogPosts: IPost[] = [
         title: "Playful Paws: Cats and Their Favorite Toys",
         content: "Embark on a journey into the playful world of cats and their favorite toys! Cats are natural hunters, and their playful instincts come to life when they interact with their beloved playthings. From feather wands that mimic prey to the elusive red dot of a laser pointer, these kitties know how to have a good time.\n\nIn this blog post, we'll showcase the joy and excitement that cats experience during playtime. Join us as we highlight some of the most popular cat toys, share heartwarming stories of feline playfulness, and explore the unique personalities that shine through when a cat engages with their favorite toys.",
         author: "PlayfulWhiskers",
-        image: "https://example.com/playfulcats3.jpg",
+        image: "https://catinaflat.blog/wp-content/uploads/2021/12/cat-g2717c217f_1920.jpg",
         date: new Date("2023-10-15"),
         category: "Cat Playtime"
     },
@@ -30,7 +30,7 @@ export const catBlogPosts: IPost[] = [
         title: "The Art of Cat Grooming: Tips for a Healthy Coat",
         content: "Cat grooming is not just about keeping your feline friend looking fabulous—it's essential for their overall health and well-being. In this blog post, we'll delve into the art of cat grooming and provide valuable tips for maintaining a healthy coat.\n\nDiscover the best grooming tools, techniques for handling even the most finicky of cats, and how regular grooming can strengthen the bond between you and your furry companion. Whether you have a long-haired beauty or a short-haired charmer, these grooming tips will leave your cat looking and feeling their best.",
         author: "GroomingGuru",
-        image: "https://example.com/catgrooming4.jpg",
+        image: "https://assets3.thrillist.com/v1/image/3059921/1200x630/flatten;crop_down;webp=auto;jpeg_quality=70",
         date: new Date("2023-09-30"),
         category: "Cat Care"
     },
@@ -38,7 +38,7 @@ export const catBlogPosts: IPost[] = [
         title: "Cats and Companionship: Understanding Feline Social Behavior",
         content: "Cats may have a reputation for being independent, but they are also social creatures that thrive on companionship. In this blog post, we'll explore the fascinating world of feline social behavior, shedding light on the intricacies of cat relationships.\n\nLearn about the importance of socialization for kittens, decipher the subtle cues cats use to communicate with each other, and discover how to strengthen the bond between you and your feline friend. Whether you have a single cat or a multi-cat household, understanding feline social behavior is key to providing a happy and fulfilling life for your furry companions.",
         author: "SocialCatWhisperer",
-        image: "https://example.com/catsocial5.jpg",
+        image: "https://images.theconversation.com/files/506718/original/file-20230127-16-q3bhc2.jpg?ixlib=rb-1.1.0&rect=21%2C0%2C6657%2C4290&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip",
         date: new Date("2023-09-18"),
         category: "Cat Behavior"
     },
@@ -46,7 +46,7 @@ export const catBlogPosts: IPost[] = [
         title: "Feline Health: Common Cat Ailments and How to Spot Them",
         content: "Keeping your cat healthy is a top priority for any cat owner. In this blog post, we'll dive into common cat ailments and provide valuable insights on how to spot them early. From subtle changes in behavior to physical symptoms, understanding the signs of illness can make a significant difference in your cat's well-being.\n\nExplore topics such as dental health, nutrition, and preventive care to keep your feline friend in top condition. With the right knowledge and proactive approach, you can ensure a long and happy life for your beloved cat.",
         author: "HealthyPaws",
-        image: "https://example.com/cathealth6.jpg",
+        image: "https://d2zp5xs5cp8zlg.cloudfront.net/image-76853-800.jpg",
         date: new Date("2023-09-05"),
         category: "Cat Health"
     },
@@ -54,7 +54,7 @@ export const catBlogPosts: IPost[] = [
         title: "Cats in Art: A Pictorial Celebration of Feline Majesty",
         content: "Cats have been a muse for artists throughout history, inspiring countless works of art that capture their grace, mystery, and majesty. In this blog post, we'll take a visual journey through the ages and explore the significant role cats have played in the world of art.\n\nFrom ancient Egyptian hieroglyphs to modern masterpieces, witness the diverse representations of cats in various art forms. Discover the symbolism behind feline depictions and gain a deeper appreciation for the timeless connection between cats and creativity.",
         author: "ArtisticWhiskers",
-        image: "https://example.com/catart7.jpg",
+        image: "https://sothebys-com.brightspotcdn.com/dims4/default/eba7f87/2147483647/strip/true/crop/2000x1131+0+273/resize/1156x654!/quality/90/?url=http%3A%2F%2Fsothebys-brightspot.s3.amazonaws.com%2Fdotcom%2F69%2F8b%2Ffc2f4cc3492094fdbe18f9bf1e15%2Fcats-charles-van-den-eycken-the-jewel-box.jpg",
         date: new Date("2023-08-22"),
         category: "Cat Culture"
     },
@@ -62,7 +62,7 @@ export const catBlogPosts: IPost[] = [
         title: "Cats and Technology: A Whisker-Approved Guide to Cat Apps",
         content: "In a world dominated by technology, even our feline friends can benefit from the wonders of the digital age. This blog post serves as your whisker-approved guide to cat apps—interactive and entertaining tools designed to engage and delight your cat.\n\nExplore a variety of apps that cater to different feline interests, from virtual mice to soothing sounds. Learn how these apps can provide mental stimulation, exercise, and relaxation for your cat. Whether you have a tech-savvy kitty or are curious about introducing your cat to the digital realm, this guide is your gateway to a purr-fectly enriched feline experience.",
         author: "TechyTabbies",
-        image: "https://example.com/catapps8.jpg",
+        image: "https://i.imgur.com/XDySO.jpg",
         date: new Date("2023-08-08"),
         category: "Cat Technology"
     },
@@ -70,7 +70,7 @@ export const catBlogPosts: IPost[] = [
         title: "Cats and Fashion: Stylish Trends for Your Feline Friend",
         content: "Fashion isn't just for humans—cats can be trendsetters too! In this blog post, we'll explore stylish trends for your feline friend, from adorable accessories to fashionable attire that showcases their unique personality.\n\nDiscover the world of cat fashion, featuring tips on choosing the right outfits, introducing accessories, and creating a wardrobe that reflects your cat's individual style. Whether you're a seasoned cat fashionista or a newcomer to the trend, this guide will inspire you to elevate your cat's look to new heights of chicness!",
         author: "FashionableFelines",
-        image: "https://example.com/catfashion9.jpg",
+        image: "https://api.time.com/wp-content/uploads/2019/08/folds-cat-fashion.jpg?quality=85&w=4800",
         date: new Date("2023-07-25"),
         category: "Cat Fashion"
     },
@@ -78,7 +78,7 @@ export const catBlogPosts: IPost[] = [
         title: "Cat Cafés: A Cozy Haven for Cat Lovers",
         content: "Step into the enchanting world of cat cafés, where coffee meets feline companionship. In this blog post, we'll explore the concept of cat cafés, offering a cozy haven for cat lovers to enjoy their favorite beverage surrounded by adorable, adoptable cats.\n\nDiscover the origin of cat cafés, the positive impact they have on feline welfare, and the unique experiences they offer to patrons. Whether you're a seasoned cat café enthusiast or considering your first visit, this post will whisk you away to a world where coffee and cats coexist in purr-fect harmony.",
         author: "CaféCatLover",
-        image: "https://example.com/catcafe10.jpg",
+        image: "https://cf.creatrip.com/original/blog/13632/2wq8m36kzvtcsuti5klhdopux2ycjbpa.jpeg",
         date: new Date("2023-07-10"),
         category: "Cat Cafés"
     },
