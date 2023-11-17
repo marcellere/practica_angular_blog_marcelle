6d460ba834f72a64223fd8e8adb893c94ed5773a
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-11-17 14:03:07 +0100

    Creado esbozo formulario para nuevo post y enlazado como FormGroup, importado el reactive forms module.

--- src/app/shared/pages/post-form/post-form.component.ts
+++ src/app/shared/pages/post-form/post-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { PostsService } from 'src/app/core/services/posts.service';
 
 @Component({
   selector: 'app-post-form',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-form.component.css']
 })
 export class PostFormComponent {
+
+  postsService = inject(PostsService)
+  arrCategories: string[] = []
+  newPost: FormGroup
+
+  constructor() {
+    this.newPost = new FormGroup({
+      title: new FormControl(),
+      content: new FormControl(),
+      author: new FormControl(),
+      author_image: new FormControl(),
+      image: new FormControl(),
+      category: new FormControl(),
+      tags: new FormControl()
+    })
+  }
+
+  ngOnInit() {
+    this.arrCategories = this.postsService.getCategories()
+  }
+
+  onSubmit() {
+    this.postsService.create(this.newPost.value)
+  }
 
 }
